b0a020379f58522696cd6d109aa5ea53b8431533
Kartik Karnwal <katikkarnwal07@gmail.com>
2026-03-25 12:34:24 +0530

    Fix password hashing bug breaking registration

--- server/models/Lawyer.js
+++ server/models/Lawyer.js
@@ -34,11 +34,10 @@ const lawyerSchema = new mongoose.Schema({
   casesHandled: { type: Number, default: 0 }
 }, { timestamps: true });
 
-lawyerSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+lawyerSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 lawyerSchema.methods.comparePassword = async function(candidatePassword) {
